6563ddf87638cfedf18769998887e68458504765
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-06-08 21:59:45 +0400

    implement search

--- client/src/app/(nondashboard)/landing/HeroSection.tsx
+++ client/src/app/(nondashboard)/landing/HeroSection.tsx
@@ -1,10 +1,49 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { setFilters } from "@/state";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { toast } from "sonner";
 
 const HeroSection = () => {
+  const dispatch = useDispatch();
+  const [searchQuery, setSearchQuery] = useState("");
+  const router = useRouter();
+
+  const handleLocationSearch = async () => {
+    try {
+      const trimmedQuery = searchQuery.trim();
+      if (!trimmedQuery) return;
+
+      const response = await fetch(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+          trimmedQuery
+        )}.json?access_token=${
+          process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+        }&fuzzyMatch=true`
+      );
+      const data = await response.json();
+      if (data.features && data.features.length > 0) {
+        const [lng, lat] = data.features[0].center;
+        dispatch(
+          setFilters({ location: trimmedQuery, coordinates: [lat, lng] })
+        );
+        const params = new URLSearchParams({
+          location: trimmedQuery,
+          lat: lat.toString(),
+          lng: lng.toString(),
+        });
+        router.push(`/search?${params.toString()}`);
+      }
+    } catch (error) {
+      console.error("Error searching location:", error);
+      toast.error("Failed to search location");
+    }
+  };
   return (
     <div className="relative h-screen">
       <Image
@@ -38,13 +77,13 @@ const HeroSection = () => {
           <div className="flex justify-center">
             <Input
               type="text"
-              value="search query"
-              onChange={() => {}}
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
               placeholder="Search by city, neighborhood, or property name"
               className="w-full max-w-lg rounded-none rounded-l-xl border-none bg-white h-12"
             />
             <Button
-              onClick={() => {}}
+              onClick={handleLocationSearch}
               className="bg-(--secondary-500) rounded-none rounded-r-lg border-none h-12 cursor-pointer hover:bg-(--secondary-600) transition-colors duration-300"
             >
               Search
